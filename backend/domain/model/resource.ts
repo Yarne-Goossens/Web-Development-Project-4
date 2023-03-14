e573fe713f11767ba72ae07538fd81012e21d3e5
Yarne-Goossens <yarne.goossens@student.ucll.be>
2023-03-14 19:40:39 +0100

    resource overview fix

--- backend/domain/model/resource.ts
+++ backend/domain/model/resource.ts
@@ -1,5 +1,5 @@
 export class Resource {
-    resource_id?: Number
+    resource_id?: number
     readonly resource_name: string
     readonly chemical_composition: string
     readonly description: string
@@ -17,7 +17,15 @@ export class Resource {
         return new Resource(resource_name,chemical_composition,description,planet_id)
     }
 
+    public getResource_id?(): number {
+        return this.resource_id;
+    }
+
+    public setResource_id?(resource_id: number): void {
+        this.resource_id = resource_id;
+    }
+
     static from(arg0: Resource) {
-        return new Resource(arg0.resource_name, arg0.chemical_composition, arg0.description, arg0.planet_id)
+        return new Resource(arg0.resource_name, arg0.chemical_composition, arg0.description, arg0.planet_id, arg0.resource_id)
     }
 }
